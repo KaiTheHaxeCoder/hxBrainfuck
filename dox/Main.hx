b727de53e17eb86e98c2af2915be19e5062be3d0
kai <bpurdy12@icloud.com>
2026-08-06 20:31:53 -0700

    api

---
+++ dox/Main.hx
@@ -0,0 +1,13 @@
+package;
+
+import hxBrainfuck.*;
+
+@:dox(hide)
+class Main
+{
+    public static function main() 
+    {
+        var bf = new Brainfuck();
+        bf.load('');
+    }
+}
---
+++ generate-docs.sh
@@ -0,0 +1,4 @@
+cd dox
+haxe build.hxml
+cd ..
+haxelib run dox -i dox -o api
